69ddcd8a76f905a1feb58c01ca11607b192e414a
Vitor Versiani <vitordversiani@gmail.com>
2022-03-20 07:48:59 -0300

    Adicionando testes com Exceptions.

--- src/main/java/br/com/alura/tdd/service/BonusService.java
+++ src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -8,10 +8,13 @@ import br.com.alura.tdd.modelo.Funcionario;
 public class BonusService {
 
 	public BigDecimal calcularBonus(Funcionario funcionario) {
-		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
+		BigDecimal valor = funcionario.getSalario()
+				.multiply(new BigDecimal("0.1"));
+
 		if (valor.compareTo(new BigDecimal("1000")) > 0) {
-			valor = BigDecimal.ZERO;
+			throw new IllegalArgumentException("Funcionário com salário maior do que R$ 10.000,00 reais não pode receber bônus.");
 		}
+
 		return valor.setScale(2, RoundingMode.HALF_UP);
 	}
 
--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -1,6 +1,6 @@
 package br.com.alura.tdd.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import br.com.alura.tdd.modelo.Funcionario;
 import org.junit.jupiter.api.Test;
@@ -13,9 +13,18 @@ public class BonusServiceTest {
     @Test
     public void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
         BonusService service = new BonusService();
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Vitor", LocalDate.now(), new BigDecimal("25000")));
 
-        assertEquals(new BigDecimal("0.00"), bonus);
+        assertThrows(IllegalArgumentException.class,
+                () -> service.calcularBonus(new Funcionario("Vitor", LocalDate.now(), new BigDecimal("25000"))));
+
+        /** Usado geralmente para capturar as mensagens de erros do Try/Catch. **/
+//        try{
+//            service.calcularBonus(new Funcionario("Vitor", LocalDate.now(), new BigDecimal("25000")));
+//            fail("Não deu o Exception!"); //Logo o teste falhou. Para forçar esse caso, comente o throw na BonusService.
+//        }catch (Exception e) {
+//            assertEquals("Funcionário com salário maior do que R$ 10.000,00 reais não pode receber bônus.", e.getMessage());
+//        }
+
     }
 
     @Test
